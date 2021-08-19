d36fec4a25a7cfdd8a2a040969c2fcde94f9b07f
JunioResende <junioresende23@gmail.com>
2021-08-19 14:55:34 -0300

    base config

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -4,10 +4,6 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  return res.json({ message: 'Hello World' });
-});
-
 app.listen(3333, () => {
   console.log('⚖️  ROAdvAPI server started on port 3333!...');
 });
