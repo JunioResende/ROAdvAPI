f71f040e3e0ddee63088b77996e9442639270f75
JunioResende <junioresende23@gmail.com>
2021-08-19 15:08:21 -0300

    swagger config

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,8 +1,13 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+
+import swaggerFile from '../../../../swagger.json';
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/ROAdvAPI-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.listen(3333, () => {
   console.log('⚖️  ROAdvAPI server started on port 3333!...');
