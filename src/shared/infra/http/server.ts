956e9079f92433c88155e2d6ec35e17e3080d046
JunioResende <junioresende23@gmail.com>
2021-08-19 13:10:51 -0300

    config eslint, prettier and editorconfig

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,13 +1,13 @@
-import express from "express"
+import express from 'express';
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 app.get('/', (req, res) => {
-  return res.json({message: 'Hello World'})
-})
+  return res.json({ message: 'Hello World' });
+});
 
 app.listen(3333, () => {
-  console.log("⚖️  ROAdvAPI server started on port 3333!...")
-})
+  console.log('⚖️  ROAdvAPI server started on port 3333!...');
+});
